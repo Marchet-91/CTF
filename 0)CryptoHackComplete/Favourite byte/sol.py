865e156de6f3b77301dc7eaa4f8dc002974cce04
RosarioSistemista <yangrosario2006@gmail.com>
2026-05-23 02:18:29 +0200

    CryptoHack

---
+++ 0)CryptoHackComplete/Favourite byte/sol.py
@@ -0,0 +1,9 @@
+from pwn import *
+
+ct = bytes.fromhex("73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d")
+
+for i in range(256):
+    tmp = xor(i, ct).decode()
+    if "crypto" in tmp:
+        print(tmp)
+        exit()
---
+++ 0)CryptoHackComplete/You either know, XOR you don't/sol.py
@@ -0,0 +1,6 @@
+from pwn import *
+
+ct = bytes.fromhex("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
+
+key = b"myXORkey"
+print(xor(ct,key).decode())
