ad815afca36f4d0144a97c42c03fef9da0e2766b
RosarioSistemista <yangrosario2006@gmail.com>
2026-05-25 11:11:52 +0200

    Condrum

---
+++ CTR-MAC/sol.py
@@ -0,0 +1,32 @@
+import os
+import signal
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from base64 import b64decode, b64encode
+from pwn import * 
+
+k1 = os.urandom(32)
+nonce = os.urandom(8)
+
+def compress(l):
+    l = [l[i:i+16] for i in range(0,len(l),16)]
+    l = [int.from_bytes(l_, 'big') for l_ in l]
+    return (sum(l) & (2**128 - 1)).to_bytes(16, 'big')
+
+def sign(data):
+    cipher = AES.new(k1, AES.MODE_CTR, nonce=nonce)
+    enc_data = cipher.encrypt(pad(data, 16))
+    compressed = compress(enc_data)
+    return compressed
+
+HOST = "ctrmac.challs.cyberchallenge.it"
+PORT = 37003
+
+print(sign(b"ciao"))
+
+# io = remote(HOST, PORT)
+
+
+# io.sendlineafter(b"")
+
+# io.close()
