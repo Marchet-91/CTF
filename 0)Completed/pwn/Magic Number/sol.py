b9a586ee5e8e047bad9db3a07c6aa51c811d76c7
RosarioSistemista <yangrosario2006@gmail.com>
2026-05-25 19:01:35 +0200

    Decrypt

---
+++ 0)Completed/pwn/Magic Number/sol.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pwn import *
+from ctypes import CDLL
+
+context.terminal = ["tmux", "splitw", "-h"]
+# context.log_level = 'error' # se non vuoi vedere i loggin
+exe = context.binary = ELF(args.EXE or './magic-number')
+lib = CDLL(exe.libc.path)
+host = args.HOST or '<host>.chall.srdnlen.it'
+port = int(args.PORT or 443)
+
+def unpack(txt):
+    return u64(txt.ljust(8, b'\x00'))
+
+def vers_libc(function):
+    return ELF(pwnlib.libcdb.search_by_symbol_offsets(function, select_index=10))
+
+def start_local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+def start_remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port, ssl=True)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return start_local(argv, *a, **kw)
+    else:
+        return start_remote(argv, *a, **kw)
+
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+# -- Exploit goes here --
+
+io = start()
+
+payload = "1179403647" # sol
+io.sendlineafter(b": ", payload.encode())
+print(io.recvline())
+
+io.interactive()
+
+
+
